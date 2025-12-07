5564889369c4302603a0a463ac4efd0feed93681
Razaan <razaanreza0705@gmail.com>
2025-12-07 15:27:46 +0600

    AuthLayout added

--- src/Routes/router.jsx
+++ src/Routes/router.jsx
@@ -1,16 +1,33 @@
-import { createBrowserRouter } from "react-router";
-import MainLayout from "../layouts/MainLayout";
-import Home from "../pages/HomePage/Home";
+import { createBrowserRouter } from 'react-router'
+import MainLayout from '../layouts/MainLayout'
+import Home from '../pages/HomePage/Home'
+import AuthLayout from '../layouts/AuthLayout'
+import Login from '../pages/Auth/Login'
+import Register from '../pages/Auth/Register'
 
 export let router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     Component: MainLayout,
     children: [
-        {
-            index: true,
-            Component: Home
-        }
-    ]
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
   },
-]);
+  {
+    path: '/',
+    Component: AuthLayout,
+    children: [
+      {
+        path: 'login',
+        Component: Login,
+      },
+      {
+        path: 'register',
+        Component: Register,
+      },
+    ],
+  },
+])
