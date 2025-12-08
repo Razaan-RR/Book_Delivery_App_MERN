07620c4f8bf665d9e12b55a90d24ede8ef74942e
Razaan <razaanreza0705@gmail.com>
2025-12-08 21:23:34 +0600

    Coverage section added

--- src/pages/HomePage/Home.jsx
+++ src/pages/HomePage/Home.jsx
@@ -1,7 +1,21 @@
+import { useEffect, useState } from 'react'
+import Coverage from './Components/Coverage'
+
 function Home() {
+  const [centerLocations, setCenterLocations] = useState([])
+
+  useEffect(() => {
+    fetch('/locations.json')
+      .then((res) => res.json())
+      .then((data) => {
+        console.log('Loaded JSON:', data) 
+        setCenterLocations(data)
+      })
+  }, [])
+
   return (
     <div>
-        Home
+      <Coverage centerLocations={centerLocations} />
     </div>
   )
 }
